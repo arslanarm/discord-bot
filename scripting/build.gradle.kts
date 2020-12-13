7e15e67ca0c917f2281510d4e902d8e2307a7844
Plony <esiredrise@gmail.com>
2020-12-14 01:52:20 +0600

    Uncommited files

---
+++ scripting/build.gradle.kts
@@ -0,0 +1,9 @@
+dependencies {
+    implementation(kotlin("stdlib"))
+    implementation("com.gitlab.kordlib:kordx.emoji:0.4.0")
+    implementation("dev.kord:kord-core:0.7.0-RC")
+    implementation("org.jetbrains.kotlin:kotlin-scripting-jvm:1.4.10")
+    implementation("org.jetbrains.kotlin:kotlin-scripting-common:1.4.10")
+    implementation("org.jetbrains.kotlin:kotlin-scripting-jvm-host:1.4.10")
+    implementation("org.jetbrains.kotlin:kotlin-scripting-dependencies:1.4.10")
+}
