be904bf90f68ea89fe0161c5bdaf38b696b79e88
Plony <esiredrise@gmail.com>
2020-12-17 08:37:44 +0600

    Online statistics added

--- src/main/kotlin/me/plony/bot/services/online.kt
+++ src/main/kotlin/me/plony/bot/services/online.kt
@@ -1,14 +1,23 @@
+import com.gitlab.kordlib.common.entity.Status
 import me.plony.processor.DiscordReceiver
 import me.plony.processor.Module
 import me.plony.processor.on
 import com.gitlab.kordlib.core.event.message.MessageCreateEvent
+import kotlinx.coroutines.flow.count
 
 @Module
 fun DiscordReceiver.online() {
-    on<MessageCreateEvent>(){
+
+    on<MessageCreateEvent> {
        if(message.content == "online"){
-        val guild = getGuild()!!
-        message.channel.createMessage(guild.memberCount.toString())
+           val guild = getGuild()!!
+           val onlineMembers = guild.members
+               .count { it.getPresence().status != Status.Offline }
+
+           message.channel.createMessage("""
+               Общее количество участников: ${guild.memberCount}
+               Онлайн: $onlineMembers
+               """.trimIndent())
        }
     }
 }
