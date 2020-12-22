dd99fed327d90852dd8019f78f9072bf26d807a7
Plony <esiredrise@gmail.com>
2020-12-23 02:57:52 +0600

    image name fixed

--- .space.kts
+++ .space.kts
@@ -2,7 +2,7 @@
 job("Build and Publish") {
     docker {
         build {}
-        push("mana-renewal.registry.jetbrains.space/p/discord-bot/discord-bot/myimage:latest") {
+        push("mana-renewal.registry.jetbrains.space/p/discord-bot/discord-bot/myimage") {
             tag = "v1"
         }
     }
