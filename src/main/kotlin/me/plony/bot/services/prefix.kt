206e58f3a31541685e3141407bf7fda11e180a9d
Plony <esiredrise@gmail.com>
2020-12-23 07:58:58 +0600

    prefixShower added

---
+++ src/main/kotlin/me/plony/bot/services/prefix.kt
@@ -0,0 +1,15 @@
+package me.plony.bot.services
+
+import dev.kord.core.event.message.MessageCreateEvent
+import me.plony.bot.utils.globals.prefix
+import me.plony.processor.DiscordReceiver
+import me.plony.processor.Module
+import me.plony.processor.on
+
+@Module("Модуль дающий возможность посмотреть на текущий префикс")
+fun DiscordReceiver.prefixShower() {
+    on<MessageCreateEvent> {
+        if (message.author?.isBot == true || message.content != "${prefix}префикс") return@on
+        message.channel.createMessage("Нынешний префикс: $prefix")
+    }
+}
